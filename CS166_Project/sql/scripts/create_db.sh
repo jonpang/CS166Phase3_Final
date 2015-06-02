0ba02ae6ef0c097d13fb6ce15b14c21c64df7b74
jonpang <jpang005@ucr.edu>
2015-06-02 13:18:20 -0700

    create_db changes
    -create_db now runs sql scripts to load data into database

--- CS166_Project/sql/scripts/create_db.sh
+++ CS166_Project/sql/scripts/create_db.sh
@@ -3,3 +3,10 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 psql -p 1234 final < $DIR/../src/create_tables.sql
 psql -p 1234 final < $DIR/../src/create_indexes.sql
 psql -p 1234 final < $DIR/../src/load_data.sql
+
+psql -p 1234 final < $DIR/../src/sql_files/connection_entry.sql
+psql -p 1234 final < $DIR/../src/sql_files/edu_details.sql
+psql -p 1234 final < $DIR/../src/sql_files/message_entry.sql
+psql -p 1234 final < $DIR/../src/sql_files/user_entry.sql
+psql -p 1234 final < $DIR/../src/sql_files/work_expr.sql
+
